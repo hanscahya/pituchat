dc7c9ababd84e450fe1f3b5be4896e48c519483a
Hans Cahya <mail@hanscahya.com>
2025-02-26 09:38:42 +0700

    fix: layout on chat page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,39 +7,26 @@ import { useChat } from '../context/ChatContext';
 import ChatPane from '../components/features/chat/ChatPane';
 
 const ChatBoxWrapper = () => {
-  const { activeChat, isActiveChatPane } = useChat();
+  const { isActiveChatPane } = useChat();
 
   return (
     <DefaultLayout>
-      <section className="flex h-full">
-        <main
-          className={`grid h-full grid-cols-12 ${
-            isActiveChatPane ? 'hidden lg:block' : 'block'
-          }`}
+      <main className={`grid h-full w-full grid-cols-10`}>
+        <section className={`col-span-2`}>
+          <ChatList />
+        </section>
+        <section
+          className={`${isActiveChatPane ? 'col-span-6' : 'col-span-8'}`}
+          style={{ backgroundColor: '#F9F9FA' }}
         >
-          <section
-            className={`col-span-12 md:col-span-5 lg:col-span-3 ${
-              activeChat ? 'hidden md:block' : 'block'
-            }`}
-          >
-            <ChatList />
-          </section>
-          <section
-            className={`col-span-12 md:col-span-7 lg:col-span-9 ${
-              activeChat ? 'block' : 'hidden md:block'
-            }`}
-            style={{ backgroundColor: '#F9F9FA' }}
-          >
-            <ChatBox />
-          </section>
-        </main>
-
-        {isActiveChatPane && (
-          <aside className="mx-auto w-full max-w-[525px] md:w-1/3 lg:w-1/4">
-            <ChatPane />
-          </aside>
-        )}
-      </section>
+          <ChatBox />
+        </section>
+        <aside
+          className={`col-span-2 ${isActiveChatPane ? 'block' : 'hidden'}`}
+        >
+          <ChatPane />
+        </aside>
+      </main>
     </DefaultLayout>
   );
 };
